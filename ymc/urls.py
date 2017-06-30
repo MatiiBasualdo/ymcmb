de0239b50055dc25590f92d579c12cbf60afb0b4
YumiBunny <yumiimiau@gmail.com>
2017-06-30 10:49:52 -0300

    nada.

--- ymc/urls.py
+++ ymc/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.video_list, name='video_list'),
+    url(r'^$', views.categoria_list, name='categoria_list'),
 ]
--- ymc/views.py
+++ ymc/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import *
+from .models import Video, Categoria
 
 def video_list(request):
     videos = Video.objects.all()
     return render(request, 'ymc/video_list.html', {'videos': videos})
 
-def usuario_list(request):
-    usuarios = Usuario.objects.all()
-    return render(request, 'ymc/video_list.html', {'usuarios': usuarios})
+def categoria_list(self):
+    categorias = self.nombre
+    return categorias
+
