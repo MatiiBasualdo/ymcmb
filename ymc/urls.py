d19329904bdf01b9c019f674ac50f395a80f1b43
gustavosorbara17 <gustydebera2013@gmail.com>
2017-06-30 14:06:12 -0300

    fix de un par de cosas

--- ymc/urls.py
+++ ymc/urls.py
@@ -1,7 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.video_list, name='video_list'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
 ]
--- ymc/views.py
+++ ymc/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Video, Categoria
+from .models import *
 
 def video_list(request):
     videos = Video.objects.all()
     categorias = Categoria.objects.all()
-    return render(request, 'ymc/video_list.html', {'categorias': categorias, 'videos': videos})
+    usuarios = Usuario.objects.all()
+    return render(request, 'ymc/video_list.html', {'usuarios': usuarios, 'categorias': categorias, 'videos': videos})
